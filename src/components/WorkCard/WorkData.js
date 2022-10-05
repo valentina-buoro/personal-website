7f5dcd95c2d1a1664e63623caa0365509f02d00b
queen-x-vee <valentinabuoro4@gmail.com>
2022-10-05 15:21:31 +0100

    second work card update

--- src/components/WorkCard/WorkData.js
+++ src/components/WorkCard/WorkData.js
@@ -7,22 +7,22 @@ const ProjectCardData = [
         imgsrc: prop1,
         title: 'Markdown Editor',
         text: 'This markdown editor was built with React and Remarkable libraries. It is useful for developers and anyone who wants to practice how to write markdown. It has a download button that downloads the result of your markdown text',
-        url: 'bigteenzmarkdown.netlify.app',
+        url: 'https://bigteenzmarkdown.netlify.app',
         github: 'https://github.com/queen-x-vee/markupeditor'
     },
     {
         imgsrc: prop2,
         title: 'Landing Page',
         text: '',
-        url: 'https://www.youtube.com',
-        github: 'https://github.com/queen-x-vee/markupeditor'
+        url: 'https://bigteenzlandingpage.netlify.app',
+        github: 'https://github.com/queen-x-vee/landing-page'
     },
     {
         imgsrc: prop3,
         title: 'Pixel Art Maker',
         text: '',
-        url: 'https://www.youtube.com',
-        github: 'https://github.com/queen-x-vee/markupeditor'
+        url: 'https://bigteenzpixelartmaker.netlify.app',
+        github: 'https://github.com/queen-x-vee/project-pixel-art-maker'
     }
 ]
 
