aba94d93771c3758d13a098677e5898a6672cd11
valentina buoro <valentinabuoro4@gmail.com>
2023-12-28 19:07:19 +0100

    footer fix

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,36 +1,38 @@
-import React from 'react'
-import '../Footer/FooterStyles.css'
-import { FaMailBulk, FaTwitter, FaLinkedin,FaGithub } from 'react-icons/fa'
+import React from "react";
+import "../Footer/FooterStyles.css";
+import { FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
 const Footer = () => {
   return (
-    <div className='footer'>
-        <div className='footer-container'>
-            <div className='left'>
-               
-          
-               <div className='phone'>
-                <h4>
-                <FaMailBulk size={20} style={{color:'#ffff', marginRight:'2rem'}} />
-                valentinabuoro4@gmail.com
-              
-                </h4>
-               
-               </div>
-
-
-            </div>
-            <div className='right'>
-                
-                <div className='social' style={{'marginTop':'2rem'}}>
-                <a href='https://github.com/queen-x-vee'><FaGithub size={30} style={{color:'#fff', marginRight:'2rem'}} /></a>
-                <a href='https://twitter.com/valentinaBuoro'><FaTwitter size={30} style={{color:'#fff', marginRight:'2rem'}} /></a>
-                <a href='https://linkedin.com/in/valentina-buoro'><FaLinkedin size={30} style={{color:'#fff', marginRight:'1rem'}} /></a>
-                </div>
-
-            </div>
+    <div className="footer">
+      <div className="footer-container">
+        <div className="left">
+          <div className="social" style={{ marginTop: "2rem" }}>
+            <a href="https://github.com/queen-x-vee">
+              <FaGithub
+                size={30}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+            </a>
+            <a href="https://twitter.com/valentinaBuoro">
+              <FaTwitter
+                size={30}
+                style={{ color: "#fff", marginRight: "2rem" }}
+              />
+            </a>
+            <a href="https://linkedin.com/in/valentina-buoro">
+              <FaLinkedin
+                size={30}
+                style={{ color: "#fff", marginRight: "1rem" }}
+              />
+            </a>
+          </div>
+          <div className="copyright">
+            <p>Created by Valentina Buoro  &copy; 2022</p>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
